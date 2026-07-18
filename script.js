d1c92e9504a2263a036ccbe74d41d759eb3404f8
Kalpanab26 <89635401+Kalpanab26@users.noreply.github.com>
2026-07-18 16:28:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,12 +6,10 @@ function secondHighest(arr) {
     for(let i = 0; i < arr.length; i++) {
 
         if(arr[i] > first) {
-
             second = first;
             first = arr[i];
-
-        } else if(arr[i] > second && arr[i] !== first) {
-
+        } 
+        else if(arr[i] > second && arr[i] !== first) {
             second = arr[i];
         }
     }
