79594b218aa132df9e5cb37e24f13950f0d7c96c
Kalpanab26 <89635401+Kalpanab26@users.noreply.github.com>
2026-07-19 15:59:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-let n = Number(prompt());
+function secondHighest(arr) {
 
-let first = -Infinity;
-let second = -Infinity;
-
-for(let i = 0; i < n; i++) {
-
-    let num = Number(prompt());
-
-    if(num > first) {
-
-        second = first;
-        first = num;
-
-    } else if(num > second && num < first) {
-
-        second = num;
+    if (arr.length < 2) {
+        return -Infinity;
     }
-}
 
-alert(second);
+    let first = -Infinity;
+    let second = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] > first) {
+
+            second = first;
+            first = arr[i];
+
+        } else if (arr[i] !== first && arr[i] > second) {
+
+            second = arr[i];
+        }
+    }
+
+    return second;
+}
